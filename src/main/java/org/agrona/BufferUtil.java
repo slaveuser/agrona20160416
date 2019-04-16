a060b9f0edc035468fe0f610849ce206118dc5fa
gmcpo <gmcpo@LAPTOP-2AD376QK>
2019-04-16 21:55:37 +0100

    Initial commit

---
+++ src/main/java/org/agrona/BufferUtil.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014-2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agrona;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Common functions across buffer implementations.
+ */
+public class BufferUtil
+{
+    public static final byte[] NULL_BYTES = "null".getBytes(StandardCharsets.UTF_8);
+    public static final ByteOrder NATIVE_BYTE_ORDER = ByteOrder.nativeOrder();
+    public static final long ARRAY_BASE_OFFSET = UnsafeAccess.UNSAFE.arrayBaseOffset(byte[].class);
+
+    /**
+     * Bounds check the access range and throw a {@link IndexOutOfBoundsException} if exceeded.
+     *
+     * @param buffer to be checked.
+     * @param index  at which the access will begin.
+     * @param length of the range accessed.
+     */
+    public static void boundsCheck(final byte[] buffer, final long index, final int length)
+    {
+        final int capacity = buffer.length;
+        final long resultingPosition = index + (long)length;
+        if (index < 0 || resultingPosition > capacity)
+        {
+            throw new IndexOutOfBoundsException(String.format("index=%d, length=%d, capacity=%d", index, length, capacity));
+        }
+    }
+
+    /**
+     * Bounds check the access range and throw a {@link IndexOutOfBoundsException} if exceeded.
+     *
+     * @param buffer to be checked.
+     * @param index  at which the access will begin.
+     * @param length of the range accessed.
+     */
+    public static void boundsCheck(final ByteBuffer buffer, final long index, final int length)
+    {
+        final int capacity = buffer.capacity();
+        final long resultingPosition = index + (long)length;
+        if (index < 0 || resultingPosition > capacity)
+        {
+            throw new IndexOutOfBoundsException(String.format("index=%d, length=%d, capacity=%d", index, length, capacity));
+        }
+    }
+
+    /**
+     * Get the address at which the underlying buffer storage begins.
+     *
+     * @param buffer that wraps the underlying storage.
+     * @return the memory address at which the buffer storage begins.
+     */
+    public static long address(final ByteBuffer buffer)
+    {
+        return ((sun.nio.ch.DirectBuffer)buffer).address();
+    }
+}
